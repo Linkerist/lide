4396e50950687cf33c92829b231b997bedbf5cc2
Linkerist <Linkerist@163.com>
2017-08-13 23:53:31 +0800

    Try to set lide root directory according to environment variable `LIDE_ROOT'.

--- lide.c
+++ lide.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define PROGRAM_NAME "lide"
 #define VERSION "0.0.1"
@@ -6,10 +8,36 @@
 #define TRUE 1
 #define FALSE 0
 
+#define LIDE_MAX_PATH_LEN 1024
+
+char Lide_Root_Dir[LIDE_MAX_PATH_LEN];
+
 void
 version(void)
 {
  printf("%s-%s\n", PROGRAM_NAME, VERSION);
+}
+
+static void
+set_lide_root(
+ void
+ //const char * argv
+)
+{
+ char * lr;
+ //char lide_root
+
+ lr = getenv("LIDE_ROOT");
+
+#ifdef LIDE_ROOT
+ if (lr == NULL
+  //&&
+ )
+  lr = LIDE_ROOT;
+#endif
+
+ if (lr != NULL)
+  strcpy(Lide_Root_Dir, lr);
 }
 
 int
@@ -30,6 +58,14 @@ do_input(void)
 int
 main(int argc, char ** argv)
 {
+ set_lide_root(
+  //argv[0]
+ );
+
+ version();
+
+ return 0;
+
  for (;;) {
   do_input();
  }
