98db965e06de1f3f5dbefa1279f15c8b8a717852
Linkerist <Linkerist@163.com>
2017-08-16 00:57:34 +0800

    8.16

--- lide.c
+++ lide.c
@@ -1,6 +1,15 @@
+#include "lide.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <termios.h>
+
+#include <curses.h>
+
+//#include <locale.h>
 
 #define PROGRAM_NAME "lide"
 #define VERSION "0.0.1"
@@ -10,12 +19,24 @@
 
 #define LIDE_MAX_PATH_LEN 1024
 
+unsigned flags[4] = {0, 0, 0, 0};
+
+#define FLAGOFF(flag) ((flag) / (sizeof(unsigned) * 8))
+
+#define FLAGS(flag) flags[FLAGOFF(flag)]
+
+#define FLAGMASK(flag) (1 << ((flag) % (sizeof(unsigned) * 8)))
+
+#define ISSET(flag) ((FLAGS(flag) & FLAGMASK(flag)) != 0)
+
 char Lide_Root_Dir[LIDE_MAX_PATH_LEN];
 
 void
 version(void)
+//lide_show_version(FILE * fp)
 {
- printf("%s-%s\n", PROGRAM_NAME, VERSION);
+ fprintf(stderr, "%s-%s\n", PROGRAM_NAME, VERSION);
+ //fprintf(fp, "%s-%s\n", PROGRAM_NAME, VERSION);
 }
 
 static void
@@ -55,14 +76,137 @@ do_input(void)
  return input;
 }
 
+static int
+main_initialize(int argc, char ** argv)
+{
+}
+
+void
+disable_extended_io(void)
+{
+ struct termios term;
+
+ tcgetattr(0, &term);
+ term.c_lflag &= ~IEXTEN;
+ term.c_oflag &= ~OPOST;
+ tcsetattr(0, TCSANOW, &term);
+}
+
+void
+disable_signals(void)
+{
+ struct termios term;
+
+ tcgetattr(0, &term);
+ term.c_lflag &= ~ISIG;
+ tcsetattr(0, TCSANOW, &term);
+}
+
+void
+enable_signals(void)
+{
+ struct termios term;
+
+ tcgetattr(0, &term);
+ term.c_lflag |= ISIG;
+ tcsetattr(0, TCSANOW, &term);
+}
+
+void
+disable_flow_control(void)
+{
+ struct termios term;
+
+ tcgetattr(0, &term);
+ term.c_iflag &= ~IXON;
+ tcsetattr(0, TCSANOW, &term);
+}
+
+void
+enable_flow_control(void)
+{
+ struct termios term;
+
+ tcgetattr(0, &term);
+ term.c_iflag |= IXON;
+ tcsetattr(0, TCSANOW, &term);
+}
+
+void
+terminal_init(void)
+{
+ //static struct termios newterm;
+ //static bool is_newterm = FALSE;
+
+ //if (is_newterm) {
+ // tcsetattr(0, TCSANOW, &newterm);
+ //} else {
+ //}
+
+ raw();
+ nonl();
+ noecho();
+ disable_extended_io();
+
+ if (!ISSET(PRESERVE))
+  disable_flow_control();
+
+ disable_signals();
+}
+
+//void
+//window_init(void)
+//{
+// topwin = newwin(2 - no_more_space(), COLS, 0, 0);
+// edit = newwin(editwinrows, COLS, 2 - no_more_space(), 0);
+// bottomwin = newwin(3 )
+//}
+
 int
 main(int argc, char ** argv)
 {
+ int optchr;
+ const struct option long_options[] = {
+  {"version", 0, NULL, 'V'},
+ };
+
+ //setlocale(LC_ALL, "");
+#if ENABLE_NLS
+ bindtextdomain(PACKAGE, LOCALEDIR);
+ textdomain(PACKAGE);
+#endif
+
+ while ((optchr = getopt_long(argc, argv, "", long_options, NULL)) != -1) {
+  switch (optchr) {
+   case 'V':
+    version();
+    exit(0);
+   default:
+    break;
+  }
+ }
+
  set_lide_root(
   //argv[0]
  );
 
- version();
+ if (argc > 1) {
+  if (!strcmp(argv[1], "--version")) {
+   version();
+   //lide_show_version(stdout);
+   fprintf(stdout, "\nUsing LIDE_ROOT=%s\n", Lide_Root_Dir);
+   exit(0);
+  }
+ }
+
+ //if (main_initialize(argc, argv) == 0)
+ // lide();
+
+ terminal_init();
+
+ curs_set(1);
+
+ window_init();
 
  return 0;
 
---
+++ lide.h
@@ -0,0 +1,8 @@
+#ifndef __LIDE_H__
+#define __LIDE_H__
+
+typedef _Bool bool;
+
+#define LIDE_ROOT_UID 0
+
+#endif
